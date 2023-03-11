35753b11950e080a5b6ec5c1fcb854ad16b52034
raf-sab <108232352+raf-sab@users.noreply.github.com>
2023-03-11 19:40:39 +0400

    Update custom_filters.py

--- newspaper/templatetags/custom_filters.py
+++ newspaper/templatetags/custom_filters.py
@@ -20,3 +20,17 @@ def censor(value, arg):
             result += vEdit[i]
 
     return result
+
+
+@register.filter(name='is_filters_uses')
+def is_filters_uses(value):
+    filters_arg = value.find('&')
+    if filters_arg != -1:
+        is_first_list_now = value.find('?')
+        if value[(is_first_list_now + 1): (is_first_list_now + 5)] == 'page':
+            return value[filters_arg:]
+        else:
+            return ('&' + value[is_first_list_now + 1:])
+    else:
+        return ''
+
